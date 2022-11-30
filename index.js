e6fb4d60494e5878d10e3b0cb45b56e3c8cef5c0
Mohamed Taha <mohamed.taha.fci@gmail.com>
2022-12-01 00:33:20 +0200

    First Commit

---
+++ index.js
@@ -0,0 +1,123 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const tiles = Array.from(document.querySelectorAll('.tile'));
+    const playerDisplay = document.querySelector('.display-player');
+    const resetButton = document.querySelector('#reset');
+    const announcer = document.querySelector('.announcer');
+
+    let board = ['', '', '', '', '', '', '', '', '']
+    // [0] [1] [2]
+    // [3] [4] [5]
+    // [6] [7] [8]
+    let currentPlayer = 'X';
+    let isGameActive = 'true';
+    
+    const PLAYERX_WON = 'PLAYERX_WON';
+    const PLAYERO_WON = 'PLAYERO_WON';
+    const TIE = 'TIE';
+
+    
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    function handleResultValidation() {
+        let roundWon = false;
+        for (let i = 0 ; i < 8 ; i++) {
+            const winCondition = winningConditions[i];
+            const a = board[winCondition[0]];
+            const b = board[winCondition[1]];
+            const c = board[winCondition[2]];
+            if (a === '' || b === '' || c === '') continue;
+            if (a === b && b === c) {
+                roundWon = true;
+                break;
+            }
+        }
+
+        if (roundWon) {
+            announce(currentPlayer === 'X' ? PLAYERX_WON : PLAYERO_WON);
+            isGameActive = false;
+            return;
+        }
+        if (!board.includes('')) {
+            announce(TIE);
+        }
+    }
+
+    const announce = (type) => {
+        switch(type) {
+            case PLAYERX_WON:
+                announcer.innerHTML = 'Player <span class="playerX">X</span> Won';
+                break;
+            case PLAYERO_WON:
+                announcer.innerHTML = 'Player <span class="playerO">O</span> Won';
+                break;
+            case TIE:
+                announcer.innerHTML = 'Tie';
+        }
+        announcer.classList.remove('hide');
+    };
+
+    const isValidAction = (tile) => {
+        if (tile.innerHTML === 'X' || tile.innerHTML === 'O') {
+            return false;
+        }
+        return true;
+    }
+
+    const updateBoard = (index) => {
+        board[index] = currentPlayer;
+    }
+
+    const changePlayer = () => {
+        playerDisplay.classList.remove(`player${currentPlayer}`);
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        playerDisplay.innerHTML = currentPlayer;
+        playerDisplay.classList.add(`player${currentPlayer}`);
+    };
+
+    const userAction = (tile, index) => {
+        console.log(index);
+        if (isValidAction(tile) && isGameActive) {
+            tile.innerText = currentPlayer;
+            tile.classList.add(`player${currentPlayer}`);
+            updateBoard(index);
+            handleResultValidation();
+            changePlayer();
+        }
+    };
+
+
+    
+    const resetBoard = () => {
+        board = ['', '', '', '', '', '', '', '', ''];
+        isGameActive = 'true';
+        announcer.classList.add('hide');
+        if (currentPlayer === 'O') {
+            changePlayer();
+        }
+        
+        for (let i = 0 ; i < tiles.length ; i++) {
+            let tile = tiles[i];
+            tile.innerText = '';
+            tile.classList.remove('playerX');
+            tile.classList.remove('playerO');
+        }
+    }
+    
+    for (let index = 0 ; index < tiles.length ; index++) {
+        tiles[index].addEventListener('click', function() {
+            userAction(tiles[index], index);
+        });
+    }
+    console.log(tiles[0]);
+
+    resetButton.addEventListener('click', resetBoard);
+});
